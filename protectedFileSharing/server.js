db4d7857571f374fba8ce7301514233fd0a87e06
2AM-official <zhaoxinglyu@icloud.com>
2022-06-30 21:41:39 -0700

    have trouble connecting mongosh

--- protectedFileSharing/server.js
+++ protectedFileSharing/server.js
@@ -1,5 +1,14 @@
+require("dotenv").config()
+const multer = require("multer")
+
 const express = require("express")
+const req = require("express/lib/request")
+const { default: mongoose } = require("mongoose")
 const app = express()
+
+const upload = multer({dest: "uploads"})
+
+mongoose.connect(process.env.DATABASE_URL)
 
 app.set("view engine", "ejs")
 
@@ -7,4 +16,8 @@ app.get("/", (req, res) => {
     res.render("index")
 })
 
-app.listen(3000)
+app.post("/upload", upload.single("file"), (req, res) => {
+    res.send("hi")
+})
+
+app.listen(process.env.DATABASE_URL)
